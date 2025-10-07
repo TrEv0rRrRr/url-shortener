e9a9d65c98f0fe9b0a0580d98f53c2352c06f822
Trevor <valentinosolischang@gmail.com>
2025-10-07 12:29:06 -0500

    feat: add Button component with customizable styles and props

---
+++ src/components/Main-components/components/ui/Button.tsx
@@ -0,0 +1,27 @@
+import type { ButtonHTMLAttributes } from "react";
+
+type Props = {
+  text: string;
+  variant?: "primary" | "secondary";
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({ variant = "primary", text, ...props }: Props) => {
+  const primaryStyles = "px-6 rounded-4xl self-center"
+  const secondaryStyles = "w-full rounded-md"
+
+  const buttonStyle = variant === "primary" ? primaryStyles : secondaryStyles
+  const buttonType = variant === "primary" ? "button" : "submit"
+
+  return (
+    <button
+      {...props}
+      className={`bg-brand-cyan text-white py-2.5 
+      text-bold cursor-pointer hover:bg-brand-cyan/80 transition-all duration-300 ease-in-out ${buttonStyle} ${props.className ?? ""}`}
+      type={props.type ?? buttonType}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
