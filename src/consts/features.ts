7f1deaf4a804bbd103995a079022aa4c9dc68ab0
Trevor <valentinosolischang@gmail.com>
2025-10-07 12:29:46 -0500

    feat: add FEATURES constant for displaying key product features with descriptions

---
+++ src/consts/features.ts
@@ -0,0 +1,20 @@
+export const FEATURES = [
+  {
+    imgSrc: "src/assets/icons/icon-brand-recognition.svg",
+    title: "Brand Recognition",
+    content:
+      "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.",
+  },
+  {
+    imgSrc: "src/assets/icons/icon-detailed-records.svg",
+    title: "Detailed Records",
+    content:
+      "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+  },
+  {
+    imgSrc: "src/assets/icons/icon-fully-customizable.svg",
+    title: "Fully Customizable",
+    content:
+      "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+  },
+];
