e30b7f6f6887b5d116d6f769edd0ea2c8c574e39
Trevor <valentinosolischang@gmail.com>
2025-10-07 12:27:35 -0500

    feat: implement main application structure with Header, Main, and Footer components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer-components/Footer";
+import Header from "./components/Header-components/Header";
+import Main from "./components/Main-components/Main";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
