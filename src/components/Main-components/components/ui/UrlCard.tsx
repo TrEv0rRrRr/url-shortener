89fb37358728c1b975dd12b535cdd2f8741a6f0b
Trevor <valentinosolischang@gmail.com>
2025-10-07 12:29:13 -0500

    feat: add UrlCard component for displaying original and shortened URLs with copy functionality

---
+++ src/components/Main-components/components/ui/UrlCard.tsx
@@ -0,0 +1,35 @@
+import { useCopyToClipboard } from '../../../../hooks/useCopyToClipboard';
+import Button from './Button';
+
+type Props = {
+  originalUrl: string;
+  shortenedUrl: string;
+}
+
+const UrlCard = ({ originalUrl, shortenedUrl }: Props) => {
+  const { handleCopy, isCopied } = useCopyToClipboard(2000, shortenedUrl);
+  const buttonText = isCopied ? "Copied!" : "Copy";
+  const buttonCopiedStyle = isCopied ? "!bg-brand-dark hover:!bg-brand-dark/80" : "";
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center text-start bg-white w-full max-w-70 rounded-md p-4">
+      <div className="border border-l-0 border-r-0 border-t-0 border-b-text-muted w-full pb-2">
+        <p className="truncate max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+          {originalUrl}
+        </p>
+      </div>
+      <div className='flex flex-col gap-4 w-full'>
+        <p className='text-brand-cyan truncate max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl'>{shortenedUrl}</p>
+        <Button
+          onClick={handleCopy}
+          text={buttonText}
+          variant='secondary'
+          type='button'
+          className={buttonCopiedStyle}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default UrlCard
