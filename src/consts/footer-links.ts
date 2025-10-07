9de98571c025b291601d81b17da0f1ce1cc8e3a2
Trevor <valentinosolischang@gmail.com>
2025-10-07 12:29:52 -0500

    feat: add FOOTER_LINKS constant for displaying footer navigation links

---
+++ src/consts/footer-links.ts
@@ -0,0 +1,14 @@
+export const FOOTER_LINKS = [
+  {
+    title: "Features",
+    links: ["Link Shortening", "Branded Links", "Analytics"],
+  },
+  {
+    title: "Resources",
+    links: ["Blog", "Developers", "Support"],
+  },
+  {
+    title: "Company",
+    links: ["About", "Our Team", "Careers", "Contact"],
+  },
+];
