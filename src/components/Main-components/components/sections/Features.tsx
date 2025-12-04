6ddf65b16d34a21bd5990e9b23d78c0a53fab951
Trevor <valentinosolischang@gmail.com>
2025-12-04 18:27:43 -0500

    feat: enhance responsiveness and styling in Features and FeatureCard components

--- src/components/Main-components/components/sections/Features.tsx
+++ src/components/Main-components/components/sections/Features.tsx
@@ -4,17 +4,17 @@ import FeatureCard from '../ui/FeatureCard'
 const Features = () => {
   return (
     <section className="flex flex-col items-center 
-    justify-center gap-22 bg-text-muted/20 pt-12 py-18">
+    justify-center gap-22 bg-text-muted/20 pt-12 py-18 xl:gap-8 xl:pb-25">
       <div className="flex flex-col items-center 
     justify-center gap-5">
         <h2 className="font-bold text-3xl">Advanced Statistics</h2>
-        <p className="text-text-secondary px-5">
+        <p className="text-text-secondary px-5 xl:px-96">
           Track how your links are performing across the web with our advanced statistics dashboard.
         </p>
       </div>
 
       <div className="flex flex-col items-center 
-    justify-center gap-25">
+    justify-center gap-25 xl:flex-row xl:gap-10">
         {
           FEATURES.map(({ content, imgSrc, title }, index) => {
             return <FeatureCard content={content} imgSrc={imgSrc} title={title} id={index} />
--- src/components/Main-components/components/ui/FeatureCard.tsx
+++ src/components/Main-components/components/ui/FeatureCard.tsx
@@ -1,3 +1,5 @@
+import { getMarginMeasure } from "../../../../helpers/getMarginMeasure.ts";
+
 type Props = {
   id: number;
   imgSrc: string;
@@ -6,14 +8,14 @@ type Props = {
 }
 
 const FeatureCard = ({ content, imgSrc, title, id }: Props) => {
-  const notThirdCardStyle = "after:content-[''] after:absolute after:bg-brand-cyan after:w-2 after:h-18 after:-bottom-18"
-  const thirdCard = 2
+  const afterStyle = "after:content-[''] after:absolute after:bg-brand-cyan after:w-2 after:h-110 after:-bottom-110 xl:after:h-2 xl:after:w-100 xl:after:bottom-0 xl:after:top-38 xl:after:-right-100"
+  const firstCard = 0
 
   return (
     <div className={`bg-white flex flex-col items-center justify-center 
-    text-center gap-5 max-w-75 p-6 pt-20 pb-8 relative rounded-md
-    ${id === thirdCard ? "" : notThirdCardStyle}`}>
-      <div className="bg-brand-dark rounded-[50%] absolute -top-10">
+    text-center gap-5 max-w-80 h-max px-6 pt-20 pb-8 relative rounded-md
+    ${id === firstCard ? afterStyle : ""} xl:text-start ${getMarginMeasure(id)}`}>
+      <div className="bg-brand-dark rounded-[50%] absolute -top-10 xl:left-9">
         <img className="p-6 object-contain w-22" src={imgSrc} alt="" aria-hidden />
       </div>
 
