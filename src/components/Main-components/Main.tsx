c90fcd1744b4babc51454ebee3479b7a669b2cba
Trevor <valentinosolischang@gmail.com>
2025-12-04 17:50:38 -0500

    feat: enhance layout responsiveness in Main component for better display on larger screens

--- src/components/Main-components/Main.tsx
+++ src/components/Main-components/Main.tsx
@@ -7,13 +7,13 @@ const Main = () => {
     <Hero />
     <Features />
     <section
-      className="flex flex-col gap-5 px-5 py-20 bg-brand-dark boost-section"
+      className="flex flex-col gap-5 px-5 py-20 bg-brand-dark boost-section xl:py-10"
     >
-      <div className="relative z-10 flex flex-col gap-5">
-        <h2 className="font-bold text-white text-[1.7rem]">
+      <div className="relative z-10 flex flex-col gap-5 xl:gap-7">
+        <h2 className="font-bold text-white text-[1.7rem] xl:text-4xl">
           Boost your links today
         </h2>
-        <Button text="Get Started" className="px-8" />
+        <Button text="Get Started" className="px-8 xl:text-2xl" />
       </div>
     </section>
   </main>;
