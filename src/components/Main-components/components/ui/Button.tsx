210b8b05c683598df9fcaaf3c4a20beff41a0582
Trevor <valentinosolischang@gmail.com>
2026-01-12 19:35:31 -0500

    fix: add missing semicolons and improve code formatting in Button component

--- src/components/Main-components/components/ui/Button.tsx
+++ src/components/Main-components/components/ui/Button.tsx
@@ -3,25 +3,27 @@ import type { ButtonHTMLAttributes } from "react";
 type Props = {
   text: string;
   variant?: "primary" | "secondary";
-} & ButtonHTMLAttributes<HTMLButtonElement>
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({ variant = "primary", text, ...props }: Props) => {
-  const primaryStyles = "px-6 rounded-4xl self-center"
-  const secondaryStyles = "w-full rounded-md"
+  const primaryStyles = "px-6 rounded-4xl self-center";
+  const secondaryStyles = "w-full rounded-md";
 
-  const buttonStyle = variant === "primary" ? primaryStyles : secondaryStyles
-  const buttonType = variant === "primary" ? "button" : "submit"
+  const buttonStyle = variant === "primary" ? primaryStyles : secondaryStyles;
+  const buttonType = variant === "primary" ? "button" : "submit";
 
   return (
     <button
       {...props}
       className={`bg-brand-cyan text-white py-2.5 
-      text-bold cursor-pointer hover:bg-brand-cyan/80 transition-all duration-300 ease-in-out ${buttonStyle} ${props.className ?? ""}`}
+      text-bold cursor-pointer hover:bg-brand-cyan/80 transition-all duration-300 ease-in-out ${buttonStyle} ${
+        props.className ?? ""
+      }`}
       type={props.type ?? buttonType}
     >
       {text}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
